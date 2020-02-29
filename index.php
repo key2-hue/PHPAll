2680c2cc3ec8030f589771859fc256c8270b936b
key2-hue <keymaeda3782@gmail.com>
2020-02-29 14:24:47 +0900

    02/29 14:24

--- index.php
+++ index.php
@@ -12,7 +12,33 @@ try {
   // $pdo->exec("insert into fruits (name, price) values ('apple', 300)");
   // echo "フルーツを登録しました！";
 
-  $db = null;
+
+  // $sql = "update fruits set price = ? where id = ?";
+  // $fruits = $pdo->prepare($sql);
+  // $fruits->execute([500,1]);
+
+  // $fruits = $pdo->prepare("insert into fruits (name, price) values (:name, :price)");
+  // $fruits->execute([':name' => 'orange', ':price' => 700]);
+  // echo $pdo->lastInsertId() . "番目のフルーツを登録しました！";
+  $fruits = $pdo->prepare("insert into fruits (name, price) values (:name, :price)");
+  // $name = 'banana';
+  // $fruits->bindParam(':name',$name, PDO::PARAM_STR);
+  // $fruits->bindValue(':price', 700,PDO::PARAM_INT);
+  // $fruits->execute();
+  // $name = 'pineapple';
+  // $fruits->bindParam(':name',$name, PDO::PARAM_STR);
+  // $fruits->bindValue(':price',1000,PDO::PARAM_INT);
+  // $fruits->execute();
+  $fruits->bindParam(':name',$name,PDO::PARAM_STR);
+  $fruits->bindParam(':price',$price,PDO::PARAM_INT);
+  $name = 'kiwi';
+  $price = 790;
+  $fruits->execute();
+  $name = 'peach';
+  $price = 200;
+  $fruits->execute();
+
+  
 } catch(PDOException $e) {
   echo "エラー発生しました";
   echo $e->getMessage();
